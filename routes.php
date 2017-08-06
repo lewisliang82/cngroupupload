5ab417020ebcdc06b3d2e0b0b4b5584507ba4fa8
lgy <lianggenyu@cric.com>
2017-08-06 15:15:35 +0800

    update

--- routes.php
+++ routes.php
@@ -4,9 +4,9 @@
  * Your package routes would go here
  */
 
-$uploadRoutes=$routeName=config('cnupload.routes_map');
+$uploadRoutes = config('cnupload.routes_map');
 
 foreach($uploadRoutes as $routeName=>$configName){
     // Route::any($routeName,['middleware'=> $middleware,'uses'=>'Ender\UEditor\UEditorController@server']);
-    Route::any($routeName, ['uses'=>'CNUpload\Upload\UploadController@server']);
+    Route::any($routeName, ['uses'=>'Lewisliang82\CNUpload\Upload\UploadController@server']);
 }
--- src/ucloud/conf.php
+++ src/ucloud/conf.php
@@ -7,6 +7,6 @@ global $UCLOUD_PUBLIC_KEY;
 global $UCLOUD_PRIVATE_KEY;
 
 $SDK_VER = '1.0.8';
-$UCLOUD_PROXY_SUFFIX    = config('cnupload.core.ucloud.proxy_suffix');;
-$UCLOUD_PUBLIC_KEY      = config('cnupload.core.ucloud.public_key');;
-$UCLOUD_PRIVATE_KEY     = config('cnupload.core.ucloud.private_key');;
+$UCLOUD_PROXY_SUFFIX    = config('cnupload.core.ucloud.proxy_suffix');
+$UCLOUD_PUBLIC_KEY      = config('cnupload.core.ucloud.public_key');
+$UCLOUD_PRIVATE_KEY     = config('cnupload.core.ucloud.private_key');
