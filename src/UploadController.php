fa21441011670185e43a899c5501cbd26b54d3d6
lgy <lianggenyu@cric.com>
2017-08-16 13:11:51 +0800

    add

--- src/UploadController.php
+++ src/UploadController.php
@@ -4,13 +4,18 @@ namespace Lewisliang82\CNUpload\Upload;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Lewisliang82\CNUpload\Upload\Ucloud\proxy;
-
+use Illuminate\Support\Facades\Crypt;
 
 class UploadController extends BaseController{
 
     public function __construct(){}
 
     public function server(Request $request){
+        $act = trim($request->input ('act'));
+        $arr_act = [];
+        if($act){
+            $arr_act = unserialize (Crypt::decrypt ($act));
+        }
 
         if (!$request->hasFile('file')) {
             return response()->json(['status'=>false, 'msg'=>'上传文件为空'], 503, [], JSON_UNESCAPED_UNICODE);
@@ -32,12 +37,21 @@ class UploadController extends BaseController{
         $buket = config('cnupload.core.ucloud.buket');
         $buketUrl = config('cnupload.core.ucloud.bucketUrl');
 
+        /**
+         * 判断是否有自定义key
+         */
+        $key = config('cnupload.core.ucloud.key');
+        if($arr_act && isset($arr_act[$key]) && $arr_act[$key]){
+            $key = $arr_act[$key] . ".". $file->getClientOriginalExtension();
+        }else{
+            $key = "upload/{$image_name}";
+        }
+
         $proxy = new proxy();
-        $key = "upload/{$image_name}";
         $ret = $proxy->UCloud_PutFile($buket, $key, $file->getPathname());
 
         if(null == $ret[1]){
-            return response()->json(['status'=>true, 'host'=>"http://{$buket}{$buketUrl}/{$key}", 'ext'=>$file->getClientOriginalExtension()], 200, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['status'=>true, 'host'=>"http://{$buket}{$buketUrl}/{$key}", 'filename'=>$key, 'ext'=>$file->getClientOriginalExtension()], 200, [], JSON_UNESCAPED_UNICODE);
         }else{
             return response()->json(['status'=>false, 'msg'=>'上传异常'], 400, [], JSON_UNESCAPED_UNICODE);
         }
